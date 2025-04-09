112c578ff21ff35a3331ae0a3689d78004c82a4e
paulinitarou <paulin.masson1@gmail.com>
2025-04-09 11:51:09 +0200

    ajout DFS

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -6,6 +6,18 @@ WIDTH = 11
 HEIGHT = 11
 dim_case = 30
 
+labyrinthe = [[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], 
+              [0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0], 
+              [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0], 
+              [1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0], 
+              [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0], 
+              [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0], 
+              [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0], 
+              [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1], 
+              [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0], 
+              [0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0], 
+              [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]]
+
 #Création de la fenêtre Tkinter
 affichage = tk.Tk()
 affichage.title("Génération d'un plateau")
@@ -30,7 +42,10 @@ def generer_n_cases_aleatoire(n):
         time.sleep(1)
         
 def direction_aleatoire(direction):
-    i = random.randint(1,len(direction))
+    if len(direction) == 1:
+        i = 0
+    else:
+        i = random.randint(0, len(direction) - 1)
     return direction[i]
       
 
@@ -53,30 +68,20 @@ def deplacementY(direction):
 
 def direction_possible(posX, posY, plateau):
     direction_possible = []
-    if posX - 1 < 0 and plateau[posX - 1][posY] == 0:
-        direction_possible += "gauche"
-    if posX + 1 < 0 and plateau[posX + 1][posY] == 0:
-        direction_possible += "droite"
-    if posY - 1 < 0 and plateau[posX][posY - 1] == 0:
-        direction_possible += "haut"
-    if posY - 1 < 0 and plateau[posX][posY + 1] == 0:
-        direction_possible += "bas"
+    if posX - 1 >= 0 and plateau[posX - 1][posY] == 0:
+        direction_possible.append("gauche")
+    if posX + 1 < len(plateau) and plateau[posX + 1][posY] == 0:
+        direction_possible.append("droite")
+    if posY - 1 >= 0 and plateau[posX][posY - 1] == 0:
+        direction_possible.append("haut")
+    if posY + 1 < len(plateau[0]) and plateau[posX][posY + 1] == 0:
+        direction_possible.append("bas")
     return direction_possible
 
-def inverser_direction(direction):
-    if direction == "gauche":
-        retour = "droite"
-    if direction == "droite":
-        retour = "gauche"
-    if direction == "haut":
-        retour = "bas"
-    if direction == "bas":
-        retour = "haut"
-    return retour
-    
 
 #Resolution DFS
 def resolDFS(plateau):
+
     posX = 0
     posY = 0
     posFinalX = WIDTH - 1
@@ -84,19 +89,28 @@ def resolDFS(plateau):
     resol = False
     pile = []
     pile.append([posX,posY])
+    plateau[posX][posY] = 2
     while not resol:
-        if direction_possible(posX, posY) == []:
-            prescedant = pile.pop()
-            posX = prescedant[0]
-            posY = prescedant[1]
+        print(posX, posY)
+        if direction_possible(posX, posY, plateau) == [] and len(pile) > 0:
+            print("a")
+            plateau[posX][posY] = 3
+            precedent = pile.pop()
+            posX = precedent[0]
+            posY = precedent[1]
         else:
-            direction = direction_aleatoire(direction_possible(posX, posY))
-            plateau[posX][posY] = -1
+            print("b")
+            direction = direction_aleatoire(direction_possible(posX, posY, plateau))
             posX = posX + deplacementX(direction)
             posY = posY + deplacementY(direction)
+            plateau[posX][posY] = 2
             pile.append([posX,posY])
-        if plateau[posX][posY] == plateau[posFinalX][posFinalY]:
+        if posX == posFinalX and posY == posFinalY:
             resol = True
+        draw_plateau(plateau)
+        affichage.update()
+        time.sleep(0.1)
+        
 
     
 
@@ -108,16 +122,19 @@ def draw_plateau(plateau):
     for y in range(HEIGHT):
         for x in range(WIDTH):
             if plateau[x][y] == 1:
+                canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="black", outline="gray")
+            elif plateau[x][y] == 0:
                 canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="white", outline="gray")
-            else :
-                if plateau[x][y] == 0:
-                    canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="red", outline="gray")
+            elif plateau[x][y] == 2:
+                canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="blue", outline="gray")
+            elif plateau[x][y] == 3:
+                canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="red", outline="gray")    
                     
 #Bouton pour lancer "generer_n_case_aleatoire"
 n = 2 #nbr de carrés
 start_button = tk.Button(affichage, text = "Générer le plateau")
 start_button.pack()
-start_button = tk.Button(affichage, text = "Resoudre en DFS", command=lambda: resolDFS())
+start_button = tk.Button(affichage, text = "Resoudre en DFS", command=lambda: resolDFS(labyrinthe))
 start_button.pack()
 
 #Lancer la boucle principale de l'application
