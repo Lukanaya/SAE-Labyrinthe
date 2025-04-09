180847d631726cd8c2c1e03e791c03ac80e86006
paulinitarou <paulin.masson1@gmail.com>
2025-04-09 10:59:17 +0200

    dfs
    dfs

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -29,6 +29,79 @@ def generer_n_cases_aleatoire(n):
         affichage.update()
         time.sleep(1)
         
+def direction_aleatoire(direction):
+    i = random.randint(1,len(direction))
+    return direction[i]
+      
+
+def deplacementX(direction):
+    valeur = 0
+    if direction == "gauche":
+        valeur = - 1
+    elif direction == "droite":
+        valeur = + 1
+    return valeur
+    
+def deplacementY(direction):
+    valeur = 0
+    if direction == "haut":
+        valeur = - 1
+    elif direction == "bas":
+        valeur = + 1
+    return valeur 
+    
+
+def direction_possible(posX, posY, plateau):
+    direction_possible = []
+    if posX - 1 < 0 and plateau[posX - 1][posY] == 0:
+        direction_possible += "gauche"
+    if posX + 1 < 0 and plateau[posX + 1][posY] == 0:
+        direction_possible += "droite"
+    if posY - 1 < 0 and plateau[posX][posY - 1] == 0:
+        direction_possible += "haut"
+    if posY - 1 < 0 and plateau[posX][posY + 1] == 0:
+        direction_possible += "bas"
+    return direction_possible
+
+def inverser_direction(direction):
+    if direction == "gauche":
+        retour = "droite"
+    if direction == "droite":
+        retour = "gauche"
+    if direction == "haut":
+        retour = "bas"
+    if direction == "bas":
+        retour = "haut"
+    return retour
+    
+
+#Resolution DFS
+def resolDFS(plateau):
+    posX = 0
+    posY = 0
+    posFinalX = WIDTH - 1
+    posFinalY = HEIGHT - 1
+    resol = False
+    pile = []
+    pile.append([posX,posY])
+    while not resol:
+        if direction_possible(posX, posY) == []:
+            prescedant = pile.pop()
+            posX = prescedant[0]
+            posY = prescedant[1]
+        else:
+            direction = direction_aleatoire(direction_possible(posX, posY))
+            plateau[posX][posY] = -1
+            posX = posX + deplacementX(direction)
+            posY = posY + deplacementY(direction)
+            pile.append([posX,posY])
+        if plateau[posX][posY] == plateau[posFinalX][posFinalY]:
+            resol = True
+
+    
+
+
+
 #Fonction pour dessiner le plateau sur le Canvas
 def draw_plateau(plateau):
     canvas.delete("all")
@@ -42,7 +115,9 @@ def draw_plateau(plateau):
                     
 #Bouton pour lancer "generer_n_case_aleatoire"
 n = 2 #nbr de carrés
-start_button = tk.Button(affichage, text = "Générer le plateau", command= lambda: generer_n_cases_aleatoire(n))
+start_button = tk.Button(affichage, text = "Générer le plateau")
+start_button.pack()
+start_button = tk.Button(affichage, text = "Resoudre en DFS", command=lambda: resolDFS())
 start_button.pack()
 
 #Lancer la boucle principale de l'application
