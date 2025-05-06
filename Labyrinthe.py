6b0034531fca5359768bb05cb572d1a16e125944
paulinitarou <paulin.masson1@gmail.com>
2025-05-06 11:53:43 +0200

    ajout tp

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -2,60 +2,14 @@ import tkinter as tk
 import random
 import time
 import math
+import copy
 
 # Dimensions du plateau/cases
-<<<<<<< Updated upstream
-WIDTH = 21 # Largeur du plateau
-HEIGHT = 21 # Hauteur du plateau
-dim_case = 30
-labyrinthe = [] # Réel plateau (Attention, vérifier qu'il nest pas vide au changement len(labyrinthe) != 0)
-=======
-WIDTH = 31 # Largeur du plateau
-HEIGHT = 31 # Hauteur du plateau
+WIDTH = 5 # Largeur du plateau
+HEIGHT = 5 # Hauteur du plateau
 dim_case = 15
 proba_mur_retire = 0.02
->>>>>>> Stashed changes
 
-affichage = tk.Tk()
-affichage.title("Génération d’un plateau")
-
-# Création du widget Canvas pour afficher le plateau
-canvas = tk.Canvas(affichage, width=WIDTH * dim_case, height=HEIGHT * dim_case)
-canvas.pack()
-
-#Fonction pour dessiner le plateau sur le Canvas
-def draw_plateau(plateau):
-    canvas.delete("all")
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            if plateau[x][y] == 1:
-                canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="black", outline="gray")
-            elif plateau[x][y] == 0:
-                canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="white", outline="gray")
-            elif plateau[x][y] == 2:
-                canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="blue", outline="gray")
-            elif plateau[x][y] == 3:
-                canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="red", outline="gray") 
-            elif plateau[x][y] == 4:
-                canvas.create_rectangle(x * dim_case, y * dim_case, (x+1) * dim_case, (y+1) * dim_case, fill="yellow", outline="gray") 
-
-# Génération de deux cases aléatoires sur le plateau
-def generer_n_case_aleatoire(n):
-    # Créer d’un plateau de taille WIDTH x HEIGHT
-    plateau = [[1 for _ in range(WIDTH)] for _ in range(HEIGHT)]
-
-    for _ in range(n):
-        x = random.randint(0,WIDTH-1) ; y = random.randint(0,HEIGHT-1)
-        while plateau[x][y] == 0:
-            x = random.randint(0,WIDTH-1) ; y = random.randint(0,HEIGHT-1)
-        plateau[x][y] = 0
-        draw_plateau(plateau)
-        # permet de faire une pause de 1 seconde entre l’affichage des deux carrés
-        affichage.update()
-        time.sleep(1)
-
-
-        
 def direction_aleatoire(direction):
     if len(direction) == 1:
         i = 0
@@ -63,7 +17,6 @@ def direction_aleatoire(direction):
         i = random.randint(0, len(direction) - 1)
     return direction[i]
       
-
 def deplacementX(direction):
     valeur = 0
     if direction == "gauche":
@@ -79,144 +32,78 @@ def deplacementY(direction):
     elif direction == "bas":
         valeur = + 1
     return valeur 
-    
+
 def direction_possible(posX, posY, plateau):
     direction_possible = []
-    if posX - 1 >= 0 and plateau[posX - 1][posY] == 0:
+    if posX - 1 >= 0 and (plateau[posX - 1][posY] == 0 or plateau[posX-1][posY] == 6):
         direction_possible.append("gauche")
-    if posX + 1 < len(plateau) and plateau[posX + 1][posY] == 0:
+    if posX + 1 < len(plateau) and (plateau[posX + 1][posY] == 0 or plateau[posX+1][posY] == 6):
         direction_possible.append("droite")
-    if posY - 1 >= 0 and plateau[posX][posY - 1] == 0:
+    if posY - 1 >= 0 and (plateau[posX][posY - 1] == 0 or plateau[posX][posY-1] == 6):
         direction_possible.append("haut")
-    if posY + 1 < len(plateau[0]) and plateau[posX][posY + 1] == 0:
+    if posY + 1 < len(plateau[0]) and (plateau[posX][posY + 1] == 0 or plateau[posX][posY+1] == 6):
         direction_possible.append("bas")
     return direction_possible
 
+def plateau_to_graphe_avec_teleporteur(plateau):
+    #Transformer le plateau en graphe
+    nodes = {}
+    #initier le dictionnaire nodes avec des valeurs vides pour chaque tuple (x,y)
+    tp1 = None
+    tp2 = None
+    for x in range(len(plateau)):
+        for y in range(len(plateau)):
+            if plateau[x][y] == 0 or plateau[x][y] == 6:  # uniquement pour les cases libres
+                nodes[(x, y)] = []
+            if plateau[x][y] == 6:
+                if tp1 is None:
+                    tp1 = (x,y)
+                else:
+                    tp2 = (x,y)
 
-#Resolution DFS
-def resolDFS(plateau):
-    print(plateau)
-    #Initialisation de la position de départ
-    posX = 0
-    posY = 0
-    #Initialisation de la position de final cherchée
-    posFinalX = WIDTH - 1
-    posFinalY = HEIGHT - 1
-    resol = False
-    #Initialisation de la pile
-    pile = []
-    pile.append([posX,posY])
-    plateau[posX][posY] = 2
-    #Tant qu'on est pas a la position final on reste dans la boucle
-    while not resol:
-        #Si il n'y a pas de direction possible et que la pile n'est pas vide, on revient en arriere
-        if len(direction_possible(posX, posY, plateau)) == 0 and len(pile) > 0:
-            plateau[posX][posY] = 3
-            precedent = pile.pop()
-            posX = precedent[0]
-            posY = precedent[1]
-        #Sinon on prend une position aléatoire et on avance
-        else:
-            pile.append([posX,posY])
-            direction = direction_aleatoire(direction_possible(posX, posY, plateau))
-            posX = posX + deplacementX(direction)
-            posY = posY + deplacementY(direction)
-            plateau[posX][posY] = 2
-        #Si la position finale est atteinte on arrete la boucle
-        if posX == posFinalX and posY == posFinalY:
-            resol = True
-            #On affiche le départ et l'arrivée pour montrer la fin de la resolution DFS
-            plateau[posX][posY] = 4
-            plateau[0][0] = 4
-        draw_plateau(plateau)
-        affichage.update()
-            
+
+    for x, y in nodes.keys():
+        if x < WIDTH - 1 and (plateau[x+1][y] == 0 or plateau[x+1][y] == 6):
+            nodes[(x, y)].append((x+1, y))
+        if y < HEIGHT - 1 and (plateau[x][y+1] == 0 or plateau[x][y+1] == 6):
+            nodes[(x, y)].append((x, y+1))
+        if x > 0 and (plateau[x-1][y] == 0 or plateau[x-1][y] == 6):
+            nodes[(x, y)].append((x-1, y))
+        if y > 0 and (plateau[x][y-1] == 0 or plateau[x][y-1] == 6):
+            nodes[(x, y)].append((x, y-1))
+        if x == tp1[0] and y == tp1[1]:
+            nodes[(x, y)].append((tp2[0], tp2[1]))
+        if x == tp2[0] and y == tp2[1]:
+            nodes[(x, y)].append((tp1[0], tp1[1]))
+    return nodes
+
 def plateau_to_graphe(plateau):
     #Transformer le plateau en graphe
     nodes = {}
     #initier le dictionnaire nodes avec des valeurs vides pour chaque tuple (x,y)
     for x in range(len(plateau)):
         for y in range(len(plateau)):
-            if plateau[x][y] == 0:  # uniquement pour les cases libres
+            if plateau[x][y] == 0 or plateau[x][y] == 6:  # uniquement pour les cases libres
                 nodes[(x, y)] = []
-                
+
+
     for x, y in nodes.keys():
-        if x < WIDTH - 1 and plateau[x+1][y] == 0:
+        if x < WIDTH - 1 and (plateau[x+1][y] == 0 or plateau[x+1][y] == 6):
             nodes[(x, y)].append((x+1, y))
-        if y < HEIGHT - 1 and plateau[x][y+1] == 0:
+        if y < HEIGHT - 1 and (plateau[x][y+1] == 0 or plateau[x][y+1] == 6):
             nodes[(x, y)].append((x, y+1))
-        if x > 0 and plateau[x-1][y] == 0:
+        if x > 0 and (plateau[x-1][y] == 0 or plateau[x-1][y] == 6):
             nodes[(x, y)].append((x-1, y))
-        if y > 0 and plateau[x][y-1] == 0:
+        if y > 0 and (plateau[x][y-1] == 0 or plateau[x][y-1] == 6):
             nodes[(x, y)].append((x, y-1))
     return nodes
-
-def generer_labyrinthe(type_resol):
-    labyrinthe = [[1 for _ in range(WIDTH)] for _ in range(HEIGHT)]
-    dir = [(1,0),(-1,0),(0,1),(0,-1)] #haut bas droite gauche (direction au hasard donc osef)
-
-    pile=[(0,0)]
-
-    #construction
-    terminer = False
-    while not terminer:
-        random.shuffle(dir)
-        #sommet de la pile
-        cy, cx = pile[-1] 
-
-        #chercher la bonne direction et se déplacer
-        trouve=False
-        for dy, dx in dir:
-            ny = cy + 2*dy
-            nx = cx + 2*dx
-            #si la zone trouvé est dans les limites du labyrinthe et qu'il est possible de passer dessus alors
-            if 0 <= nx < WIDTH and 0 <= ny < HEIGHT and labyrinthe[ny][nx] != 0:
-                labyrinthe[cy + dy][cx+dx] = 0
-                labyrinthe[ny][nx] = 0
-                pile.append((ny,nx))
-                trouve=True
-                break
-
-        #On sort de la boucle
-        #On vérifie si le chemin n'a pas été trouvé
-        if not trouve:
-            pile.pop() 
-
-
-        #On vérifie si il n'y a plus de possibilité
-        if len(pile)==0: terminer=True
-
-        draw_plateau(labyrinthe)
-        # permet de faire une pause de 1 seconde entre l’affichage des deux carrés
-        affichage.update()
-        # time.sleep(0.0000001)
-
-    nb_mur=0
-    for largeur in range(len(labyrinthe)):
-        for hauteur in range(len(labyrinthe)):
-            if labyrinthe[largeur][hauteur]==1:
-                nb_mur+=1
-    nbNotMur=math.floor(0.02*nb_mur)
-    while nbNotMur>0:
-        x=random.randint(0,WIDTH-1)
-        y=random.randint(0,HEIGHT-1)
-        if labyrinthe[x][y]==1:
-            labyrinthe[x][y]=0
-            nbNotMur-=1
-        draw_plateau(labyrinthe)
-        affichage.update()
         
-    match type_resol:
-        case "DFS":
-            resolDFS(labyrinthe)
-             
-start_button = tk.Button(affichage, text="Générer le plateau et résolution par DFS", command = lambda: generer_labyrinthe("DFS"))
-start_button.pack()
+def distance(pointDepart: tuple, pointArrivee: tuple) -> float:
+    x1, y1 = pointDepart
+    x2, y2 = pointArrivee
+    resultat = math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
+    return resultat
 
-<<<<<<< Updated upstream
-# Lancer la boucle principale de l’application
-affichage.mainloop() # permet de garder la fenêtre ouverte en attendant lesinteractions de l’utilisateur
-=======
 class Labyrinthe:
     def __init__(self):
         self.labyrinthe = []
@@ -298,7 +185,7 @@ class Labyrinthe:
         result = {}
         Q = []
         #Transformer le plateau en graphe
-        nodes = plateau_to_graphe(plateau)
+        nodes = plateau_to_graphe_avec_teleporteur(plateau)
         
         sommetInitial = (0, 0)
         #Trouver le chemin le plus court dans le graphe
@@ -442,12 +329,13 @@ class Labyrinthe:
             #self.draw_plateau(plateau)
             #self.affichage.update()
         teleport = 0
-        while teleport != 2:
+        while teleport < 2:
             x=random.randint(0,WIDTH-1)
             y=random.randint(0,HEIGHT-1)
             if plateau[x][y]==0:
-                plateau[x][y] = 6
+                plateau[x][y]=6
                 teleport += 1
+       
         self.draw_plateau(plateau)
         self.affichage.update()
         return plateau
@@ -472,4 +360,3 @@ class Labyrinthe:
 
 app = Labyrinthe()
 app.run()
->>>>>>> Stashed changes
